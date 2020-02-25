3277eb65da0ff0513a501b591255b9c48fea3132
YanDushkin <42182463+YanDushkin@users.noreply.github.com>
2020-02-25 20:23:53 +0700

    Был изменён способ получения информации из файлов с вариантами; теперь для этого необходим разделитель "|". Вид xlsx файла был изменён.

--- code.py
+++ code.py
@@ -85,20 +85,7 @@ class InfoWidget(QWidget, Ui_info_window):
 
 Внешний вид варианта в папке variants(demo.txt):
 
-ТЮЛЕНЬ
-ОБЛАКО
-18
-8
-10
-5
-7413265 
-570
-10
-35
-ОВСЯНИКОВ
-16
-
-БУДЬТЕ ОСТОРОЖНЫ С ВВОДОМ ДАННЫХ. ПРОГРАММА РЕГИСТРОЧУВСТВИТЕЛЬНАЯ.
+ТЮЛЕНЬ|ОБЛАКО|18|8|10|5|7413265|570|10|35|ОВСЯНИКОВ|16
 
 В папке program хранится exe-файл."""
 
@@ -140,6 +127,32 @@ class ExamCheckerWidget(QWidget, Ui_TableForm):
 
         self.load_table()
 
+    def get_variant_data(self, student_index):
+        variant = self.balls_tablewidget.item(student_index, 1).text()
+        variant_path = "variants/" + variant + ".txt"
+        if not os.path.exists(variant_path):
+            QMessageBox.critical(
+                self, "Ошибка", f'Варианта "{variant}" не существует (строка {student_index + 1})',
+                QMessageBox.Ok
+            )
+            return
+        try:
+            with open(variant_path, mode="r", encoding="utf-8") as variant_file:
+                variant_text = "".join(
+                    filter(lambda x: x.isalnum() or x == "|", variant_file.read())
+                )
+                variant_data = variant_text.split("|")
+                variant_data = list(map(lambda x: x.lower(), variant_data))
+                if len(variant_data) != 12:
+                    QMessageBox.critical(
+                        self, "Ошибка", "Неправильные данные в файле", QMessageBox.Ok
+                    )
+                    return
+        except OSError as error:
+            QMessageBox.critical(self, "Ошибка", error.strerror, QMessageBox.Ok)
+            return
+        return variant_data
+
     def load_table(self):
         filename = "-".join([str(self.school_number), self.teacher_initials, self.date]) + ".db"
         if not os.path.exists("balls_databases/" + filename):
@@ -158,25 +171,10 @@ class ExamCheckerWidget(QWidget, Ui_TableForm):
         if not os.path.exists("variants"):
             os.mkdir("variants")
         for i in range(self.balls_tablewidget.rowCount()):
-            variant = self.balls_tablewidget.item(i, 1).text()
-            variant_path = "variants/" + variant + ".txt"
-            if not os.path.exists(variant_path):
-                QMessageBox.critical(
-                    self, "Ошибка", f'Варианта "{variant}" не существует (строка {i + 1})',
-                    QMessageBox.Ok
-                )
-                return
-            try:
-                with open(variant_path, mode="r", encoding="utf-8") as variant_file:
-                    variant_data = variant_file.read().split("\n")
-                    variant_data = list(map(lambda x: x.strip(), variant_data))
-            except OSError as error:
-                QMessageBox.critical(self, "Ошибка", error.strerror, QMessageBox.Ok)
-                return
-
+            variant_data = self.get_variant_data(i)
             all_tasks_data = []
             for j in range(3, self.balls_tablewidget.columnCount() - 2):
-                all_tasks_data.append(self.balls_tablewidget.item(i, j).text())
+                all_tasks_data.append(self.balls_tablewidget.item(i, j).text().lower())
 
             balls = 0
             for j in range(12):
@@ -367,6 +365,7 @@ class ExamCheckerWidget(QWidget, Ui_TableForm):
         filename = "-".join([str(self.school_number), self.teacher_initials, self.date]) + ".xlsx"
         if os.path.exists(path + filename):
             os.remove(path + filename)
+
         workbook = xlsxwriter.Workbook(path + filename)
         worksheet = workbook.add_worksheet()
 
@@ -374,8 +373,41 @@ class ExamCheckerWidget(QWidget, Ui_TableForm):
             worksheet.write(0, j, self.balls_tablewidget.horizontalHeaderItem(j).text())
 
         for i in range(self.balls_tablewidget.rowCount()):
+            all_tasks_data = []
+            variant_data = self.get_variant_data(i)
             for j in range(self.balls_tablewidget.columnCount()):
-                worksheet.write(i + 1, j, self.balls_tablewidget.item(i, j).text())
+                worksheet.write(2 * i + 1, j, self.balls_tablewidget.item(i, j).text())
+                if 3 <= j < self.balls_tablewidget.columnCount() - 2:
+                    all_tasks_data.append(self.balls_tablewidget.item(i, j).text().lower())
+
+            pass_balls = []
+            for j in range(12):
+                pass_balls.append(1 if variant_data[j] == all_tasks_data[j] else 0)
+
+            part_2_data = []
+            for j in range(12, 19):
+                if all_tasks_data[j] == "0б" or all_tasks_data[j] == "0":
+                    part_2_data.append(0)
+                elif all_tasks_data[j] == "1б" or all_tasks_data[j] == "1":
+                    part_2_data.append(1)
+                elif (all_tasks_data[j] == "2б" or all_tasks_data[j] == "2") and not 14 <= j <= 16:
+                    part_2_data.append(2)
+                else:
+                    part_2_data.append(0)
+
+            task13_ball = [part_2_data[0], part_2_data[1]]
+            task14_ball = [part_2_data[2], part_2_data[3], part_2_data[4]]
+            task15_ball = [part_2_data[5], part_2_data[6]]
+
+            task13_ball = [max(task13_ball)] * 2
+            task15_ball = [max(task15_ball)] * 2
+
+            pass_balls.extend(task13_ball)
+            pass_balls.extend(task14_ball)
+            pass_balls.extend(task15_ball)
+
+            for j in range(3, self.balls_tablewidget.columnCount() - 2):
+                worksheet.write(2 * i + 2, j, pass_balls[j - 3])
 
         balls_count_list = [0] * 20
         marks_count_dict = {"2": 0, "3": 0, "4": 0, "5": 0}
@@ -386,17 +418,17 @@ class ExamCheckerWidget(QWidget, Ui_TableForm):
             balls_count_list[int(ball)] += 1
 
         worksheet.write_row(
-            self.balls_tablewidget.rowCount() + 2, 0, ["Оценка", "Частота встречаемости оценки"]
+            self.balls_tablewidget.rowCount() * 2 + 5, 0, ["Оценка", "Частота встречаемости оценки"]
         )
         for i, (k, v) in enumerate(marks_count_dict.items()):
-            worksheet.write_row(self.balls_tablewidget.rowCount() + i + 3, 0, [k, v])
+            worksheet.write_row(self.balls_tablewidget.rowCount() * 2 + i + 6, 0, [k, v])
 
         worksheet.write_row(
-            self.balls_tablewidget.rowCount() + 2, 4, ["Балл", "Частота встречаемости балла"]
+            self.balls_tablewidget.rowCount() * 2 + 5, 4, ["Балл", "Частота встречаемости балла"]
         )
         for i, count in enumerate(balls_count_list):
             worksheet.write_row(
-                self.balls_tablewidget.rowCount() + i + 3, 4, [str(i), count]
+                self.balls_tablewidget.rowCount() * 2 + i + 6, 4, [str(i), count]
             )
         workbook.close()
 
